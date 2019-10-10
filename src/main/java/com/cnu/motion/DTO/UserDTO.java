a46ea7e5c560db1699ffffc2157e3d37bd7d8217
Gayeon <kgy1559@gmail.com>
2019-10-10 23:37:13 +0900

    Create UserDTO to handle user info

---
+++ src/main/java/com/cnu/motion/DTO/UserDTO.java
@@ -0,0 +1,15 @@
+package com.cnu.motion.DTO;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String userId;
+    private String userPwd;
+    private String userName;
+    private String joinDate;
+    private String userPhone;
+    private String userEmail;
+    private String userBirth;
+    private String userRoll;
+}
