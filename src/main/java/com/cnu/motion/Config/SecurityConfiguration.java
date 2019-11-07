06fa32b3b4150134055fdf3da11d4337544d123d
Gayeon <kgy1559@gmail.com>
2019-11-07 21:30:22 +0900

    Edit security policy

--- src/main/java/com/cnu/motion/Config/SecurityConfiguration.java
+++ src/main/java/com/cnu/motion/Config/SecurityConfiguration.java
@@ -1,13 +1,28 @@
-package com.cnu.motion.Config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-    }
-}
+//package com.cnu.motion.Config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@EnableWebSecurity
+//@Configuration
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.cors().and().csrf().disable();
+////            .authorizeRequests()
+////                .antMatchers(HttpMethod.OPTIONS,"http://localhost:3000").permitAll()//allow CORS option calls
+////                .antMatchers("/resources/**").permitAll()
+////                .anyRequest().authenticated();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
